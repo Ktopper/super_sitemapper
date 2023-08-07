83d091bacacd70dce5c07d718c64f488c0f346a8
Ktop <ktopdunham@gmail.com>
2023-08-07 02:06:52 -0400

    fixed

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let browser;
 
 async function createWindow() {
   browser = await puppeteer.launch();
-  
+
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -27,15 +27,41 @@ app.on('window-all-closed', async () => {
   app.quit();
 });
 
+async function crawlPage(page, url, depth = 1, maxDepth = 2) {
+    if (depth > maxDepth) {
+      return [];
+    }
+  
+    await page.goto(url, { waitUntil: 'networkidle2' });
+  
+    // Extract all links from the current page
+    const linksOnPage = await page.$$eval('a', as => as.map(a => a.href));
+  
+    // Filter out only the internal links
+    const internalLinks = linksOnPage.filter(link => link.startsWith(url));
+  
+    let childLinks = [];
+    for (const link of internalLinks) {
+      // This will prevent the crawler from visiting a link it has already visited
+      if (!childLinks.includes(link)) {
+        childLinks.push(...await crawlPage(page, link, depth + 1, maxDepth));
+      }
+    }
+  
+    return [...internalLinks, ...childLinks];
+  }
+  
+
 ipcMain.on('generate-sitemap', async (event, url) => {
   try {
     const page = await browser.newPage();
-    await page.goto(url);
-    
-    const links = await page.$$eval('a', as => as.map(a => a.href));
-    event.reply('sitemap-response', links);
-    
+
+    const links = await crawlPage(page, url);
+
     await page.close();
+    
+    event.reply('sitemap-response', links);
+
   } catch (error) {
     event.reply('sitemap-response', null, error.toString());
   }
